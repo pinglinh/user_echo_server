3a9a57a03671c7fcbeddb1663df0c81c96fa8f4d
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-12-06 01:04:35 +0000

    Refactored code into STDIN and STDOUT

--- lib/echo_server.rb
+++ lib/echo_server.rb
@@ -1,49 +1,39 @@
-class UserEcho
-  def initialize(view)
-    @view = view
-  end
+class UserEchoServer
+   def initialize(input=STDIN, output=STDOUT)
+      @input = input
+      @output = output
+      @view = View.new(@output)
+   end
 
-  def start
-    @view.print_welcome_message
-    @view.print_instruction_message
-    echo_input
-  end
+   def echo_server_start
+      @view.welcome_and_instruction_message
+      @user_input = @input.readline.chomp
+      while exit_entered? == false
+        @output.puts @user_input
+        @user_input = @input.readline.chomp
+      end
+      @view.goodbye_message
+   end
 
- private
-
-  def echo_input
-    @user_input = STDIN.readline.chomp
-    while !exit_entered?
-      @view.print_user_input(@user_input)
-      @user_input = STDIN.readline.chomp
+    def exit_entered?
+        @user_input == "exit"
     end
-    @view.print_goodbye_message
-  end
-
-  def exit_entered?
-    @user_input == "exit"
-  end
 end
 
 class View
-  def print_welcome_message
-    print "Welcome!\n"
-  end
+    def initialize(output=STDOUT)
+       @output = output
+    end
 
-  def print_instruction_message
-    print "Please enter a word to be reprinted, to exit this program please enter exit:\n"
-  end
+    def welcome_and_instruction_message
+        @output.print "Welcome!\n"
+        @output.print "Enter anything or exit to finish:\n"
+    end
 
-  def print_goodbye_message
-    print "Goodbye!\n"
-  end
-
-  def print_user_input(user_input)
-    print user_input + "\n"
-  end
+    def goodbye_message
+        @output.print "Goodbye!\n"
+    end
 end
 
-
-# user_view = View.new
-# user_echo = UserEcho.new(user_view)
-# user_echo.start
+server = UserEchoServer.new
+server.echo_server_start
