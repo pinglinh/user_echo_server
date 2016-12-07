ff2f6b07ef4e40a96b2f589a0bb67733668ce4db
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-12-07 17:03:27 +0000

    Fixed indentation

--- lib/echo_server.rb
+++ lib/echo_server.rb
@@ -1,38 +1,38 @@
 class UserEchoServer
-   def initialize(input=STDIN, output=STDOUT)
-      @input = input
-      @output = output
-      @view = View.new(@output)
-   end
+  def initialize(input=STDIN, output=STDOUT)
+    @input = input
+    @output = output
+    @view = View.new(@output)
+  end
 
-   def echo_server_start
-      @view.welcome_and_instruction_message
+  def echo_server_start
+    @view.welcome_and_instruction_message
+    @user_input = @input.readline.chomp
+    while exit_entered? == false
+      @output.puts @user_input
       @user_input = @input.readline.chomp
-      while exit_entered? == false
-        @output.puts @user_input
-        @user_input = @input.readline.chomp
-      end
-      @view.goodbye_message
-   end
-
-    def exit_entered?
-        @user_input == "exit"
     end
+    @view.goodbye_message
+  end
+
+  def exit_entered?
+    @user_input == "exit"
+  end
 end
 
 class View
-    def initialize(output=STDOUT)
-       @output = output
-    end
+  def initialize(output=STDOUT)
+    @output = output
+  end
 
-    def welcome_and_instruction_message
-        @output.print "Welcome!\n"
-        @output.print "Enter anything or exit to finish:\n"
-    end
+  def welcome_and_instruction_message
+    @output.print "Welcome!\n"
+    @output.print "Enter anything or exit to finish:\n"
+  end
 
-    def goodbye_message
-        @output.print "Goodbye!\n"
-    end
+  def goodbye_message
+    @output.print "Goodbye!\n"
+  end
 end
 
 # server = UserEchoServer.new
