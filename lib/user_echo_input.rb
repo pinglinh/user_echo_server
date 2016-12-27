2e888dcd072ccd38e94d637554e09f3f359a7bc3
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-12-27 23:41:43 +0000

    Moved EOF rescue to UserEchoInput class

--- lib/user_echo_input.rb
+++ lib/user_echo_input.rb
@@ -1,9 +1,13 @@
 class UserEchoInput
+  EXIT = "exit"
+
   def initialize(input=STDIN)
     @input = input
   end
 
   def read_line
     @input.readline.chomp
+  rescue EOFError
+    EXIT
   end
 end
