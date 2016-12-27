a33860670aa02422de1590f0af12983af86f0d9f
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-12-27 02:35:40 +0000

    Modified readline method and initialized input class

--- lib/user_echo_server.rb
+++ lib/user_echo_server.rb
@@ -1,4 +1,5 @@
-require 'command_line_view'
+require_relative 'command_line_view'
+require_relative 'user_echo_input'
 
 class UserEchoServer
   EXIT = "exit"
@@ -7,17 +8,18 @@ class UserEchoServer
     @input = input
     @output = output
     @command_line_view = CommandLineView.new(@output)
+    @user_echo_input = UserEchoInput.new(@input)
   end
 
   def start
     @command_line_view.welcome_message
     @command_line_view.instruction_message
-    @user_input = readline
-    while exit_entered? == true
-      @command_line_view.goodbye_message
+    @user_echo_input.read_line
+    while exit_entered? == false
+      @command_line_view.echo_user_line
+      @user_echo_input.read_line
     end
-    @command_line_view.echo_user_line
-    @user_input = readline
+    @command_line_view.goodbye_message
   end
 
   private
