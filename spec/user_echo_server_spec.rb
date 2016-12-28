b8fba71da7e3b677474da8d18b2fc3e75d700a16
Linh Nguyen My <lnguyenmy1@gmail.com>
2016-12-28 00:24:34 +0000

    Added extra tests

--- spec/user_echo_server_spec.rb
+++ spec/user_echo_server_spec.rb
@@ -40,32 +40,44 @@ describe UserEchoServer do
     expect(line).to eq("hello")
   end
 
-  # it "echos what the user wrote" do
-  #   input = StringIO.new("one\ntwo\nthree\n")
-  #   output = StringIO.new("")
-  #   server = UserEchoServer.new(input, output)
-  #   server.start
-  #   expect(output.string).to eq(
-  #     "Welcome!\n"+
-  #     "Enter anything or exit to finish:\n"+
-  #     "one\n"+
-  #     "two\n"+
-  #     "three\n"+
-  #     "Goodbye!\n"
-  #   )
-  # end
+  it "echos what the user wrote" do
+    input = StringIO.new("one\ntwo\nthree\n")
+    output = StringIO.new("")
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n"+
+      "Enter anything or exit to finish:\n"+
+      "one\n"+
+      "two\n"+
+      "three\n"+
+      "Goodbye!\n"
+    )
+  end
 
-  # it "ends the server when user enters exit" do
-  #     input = StringIO.new("one\ntwo\nexit\nfour\n")
-  #     output = StringIO.new("")
-  #     server = UserEchoServer.new(input, output)
-  #     server.echo_server_start
-  #     expect(output.string).to eq(
-  #       "Welcome!\n"+
-  #       "Enter anything or exit to finish:\n"+
-  #       "one\n"+
-  #       "two\n"+
-  #       "Goodbye!\n"
-  #     )
-  # end
+  it "ends the server when user enters exit" do
+    input = StringIO.new("one\ntwo\nexit\nfour\n")
+    output = StringIO.new("")
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n"+
+      "Enter anything or exit to finish:\n"+
+      "one\n"+
+      "two\n"+
+      "Goodbye!\n"
+    )
+  end
+
+  it 'rescues EOF' do
+    input = StringIO.new("")
+    output = StringIO.new("")
+    server = UserEchoServer.new(input, output)
+    server.start
+    expect(output.string).to eq(
+      "Welcome!\n"+
+      "Enter anything or exit to finish:\n"+
+      "Goodbye!\n"
+    )
+  end
 end
